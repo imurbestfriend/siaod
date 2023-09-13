972e075cf383cb1f572ca15f44bbb05a810b1ab0
imurbestfriend <144876946+imurbestfriend@users.noreply.github.com>
2023-09-13 08:49:50 +0300

    Create fourth.cpp

---
+++ firstPrac/fourth.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bitset>
+
+int main()
+{
+	int number;
+	std::cout << "Enter number: ";
+	std::cin >> number;
+
+	std::cout << std::bitset<32>(number) << "\n";
+	number = number >> 3;
+	std::cout << std::bitset<32>(number) << "\n";
+	std::cout << "Result: " << number;
+}
