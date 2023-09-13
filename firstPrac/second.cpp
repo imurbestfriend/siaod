e9439d0ffb7b6c55096e06179d940f2ab3bb4ba8
imurbestfriend <144876946+imurbestfriend@users.noreply.github.com>
2023-09-13 08:47:15 +0300

    Update second.cpp

--- firstPrac/second.cpp
+++ firstPrac/second.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-    short int number = 0xFF12; // 1111_1111_0001_0010
-    short int mask = 0xC07F; // 1100_0011_1111_1111
+    short int number = 0xFF12; 
+    short int mask = 0xC07F; 
 
     std::cout << "Number: \t" << std::bitset<16>(number) << "\n";
     std::cout << "Mask:   \t" << std::bitset<16>(mask) << "\n";
